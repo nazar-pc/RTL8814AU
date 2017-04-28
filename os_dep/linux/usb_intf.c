9c465d48190fa22e49b2ce056fc1676afd41d2a9
jesuisjd <jesuisjd1@gmail.com>
2017-04-28 18:53:10 -0400

    Update usb_intf.c
    Added support for TRENDnet AC1900 usb wireless adapter which has RTL8814AU chipset in it

--- os_dep/linux/usb_intf.c
+++ os_dep/linux/usb_intf.c
@@ -212,6 +212,7 @@ static struct usb_device_id rtw_usb_id_tbl[] ={
 	{USB_DEVICE(0x7392, 0xA834), .driver_info = RTL8814A}, /* Edimax - Edimax */
 	{USB_DEVICE(0x7392, 0xA833), .driver_info = RTL8814A}, /* Edimax - Edimax */
 	{USB_DEVICE(0x2357, 0x0106), .driver_info = RTL8814A}, /* TP-LINK - TP-LINK */
+	{USB_DEVICE(0x20f4, 0x809a), .driver_info = RTL8814A}, /* TRENDnet - TRENDnet */
 #endif /* CONFIG_RTL8814A */
 
 #ifdef CONFIG_RTL8188F
