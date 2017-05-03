fd8eb1e9c589909d76ceb030bd8b2fbd1815202b
Hans Ulli Kroll <ulli.kroll@googlemail.com>
2017-05-03 18:13:33 +0200

    add USB id for TRENDnet - TAC1900
    Signed-off-by: Hans Ulli Kroll <ulli.kroll@googlemail.com>

--- os_dep/linux/usb_intf.c
+++ os_dep/linux/usb_intf.c
@@ -211,6 +211,7 @@ static struct usb_device_id rtw_usb_id_tbl[] ={
 	{USB_DEVICE(0x056E, 0x400D), .driver_info = RTL8814A}, /* ELECOM - ELECOM */
 	{USB_DEVICE(0x7392, 0xA834), .driver_info = RTL8814A}, /* Edimax - Edimax */
 	{USB_DEVICE(0x7392, 0xA833), .driver_info = RTL8814A}, /* Edimax - Edimax */
+	{USB_DEVICE(0x20f4, 0x809a), .driver_info = RTL8814A}, /* TRENDnet - TAC1900 */
 #endif /* CONFIG_RTL8814A */
 
 #ifdef CONFIG_RTL8188F
