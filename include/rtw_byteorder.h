cf7994db05753b45036dd707eb3c8f99c20a022a
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2017-10-02 10:01:10 +0300

    Use endian macros from kernel
    Signed-off-by: Nazar Mokrynskyi <nazar@mokrynskyi.com>

--- include/rtw_byteorder.h
+++ include/rtw_byteorder.h
@@ -21,16 +21,10 @@
 #define _RTL871X_BYTEORDER_H_
 
 
-#if defined (CONFIG_LITTLE_ENDIAN) && defined (CONFIG_BIG_ENDIAN)
-#error "Shall be CONFIG_LITTLE_ENDIAN or CONFIG_BIG_ENDIAN, but not both!\n"
-#endif
-
-#if defined (CONFIG_LITTLE_ENDIAN)
-#  include <byteorder/little_endian.h>
-#elif defined (CONFIG_BIG_ENDIAN)
-#  include <byteorder/big_endian.h>
+#if defined(__LITTLE_ENDIAN)
+#include <byteorder/little_endian.h>
 #else
-#  error "Must be LITTLE/BIG Endian Host"
+#include <byteorder/big_endian.h>
 #endif
 
 #endif /* _RTL871X_BYTEORDER_H_ */
