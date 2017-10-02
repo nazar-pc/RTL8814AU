3fb1cd5bad9c3a2d75e554e7f3bc191e7553a8a1
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2017-10-02 10:06:05 +0300

    Added support for Netgear A7000 and TRENDnet TEW-809UB. Clarified exact model of the adapter.
    Signed-off-by: Nazar Mokrynskyi <nazar@mokrynskyi.com>

--- os_dep/linux/usb_intf.c
+++ os_dep/linux/usb_intf.c
@@ -178,9 +178,11 @@ static struct usb_device_id rtw_usb_id_tbl[] ={
 	{USB_DEVICE(0x056E, 0x400B), .driver_info = RTL8814A}, /* ELECOM - ELECOM */
 	{USB_DEVICE(0x056E, 0x400D), .driver_info = RTL8814A}, /* ELECOM - ELECOM */
 	{USB_DEVICE(0x7392, 0xA834), .driver_info = RTL8814A}, /* Edimax - Edimax */
-	{USB_DEVICE(0x7392, 0xA833), .driver_info = RTL8814A}, /* Edimax - Edimax */
+	{USB_DEVICE(0x7392, 0xA833), .driver_info = RTL8814A}, /* Edimax - AC1750 */
 	{USB_DEVICE(0x20f4, 0x809a), .driver_info = RTL8814A}, /* TRENDnet - TAC1900 */
 	{USB_DEVICE(0x2357, 0x0106), .driver_info = RTL8814A}, /* TP-Link - T9UH */
+	{USB_DEVICE(0x0846, 0x9054), .driver_info = RTL8814A}, /* Netgear - A7000 */
+	{USB_DEVICE(0x20f4, 0x809b), .driver_info = RTL8814A}, /* TRENDnet - TEW-809UB */
 #endif /* CONFIG_RTL8814A */
 
 	{}	/* Terminating entry */
